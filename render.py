19595945e37e47d1fcc6c46234f7cc7981b69e9c
Eugen <ieugenor@gmail.com>
2019-07-15 22:55:04 +0300

    add common classes

--- render.py
+++ render.py
@@ -1,6 +1,40 @@
-class MapRenderer:
-    def __init__(self, surface):
+import pygame
+
+
+class Renderer:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GRAY = (125, 125, 125)
+    LIGHT_BLUE = (64, 128, 255)
+    GREEN = (0, 200, 64)
+    GREEN_DARK = (0, 255, 0)
+    YELLOW = (225, 225, 0)
+    PINK = (230, 50, 230)
+
+    def __init__(self, surface, game_size):
         self.surface = surface
 
-    def render(self, m):
-        pass
+        self.width = surface.get_width()
+        self.height = surface.get_height
+
+        self.columns = game_size[0]
+        self.lines = game_size[1]
+
+        self.cell_width = self.width / self.columns
+        self.cell_height = self.height / self.lines
+
+    def set_cell(self, color, column, line):
+        pygame.draw.rect(self.surface, color, (column * self.cell_width,
+                                               (self.lines - line) * self.cell_height - self.cell_height,
+                                               self.cell_width, self.cell_height))
+
+    def render(self, game_map):
+        self.surface.fill((0, 0, 0))
+
+        columns, lines = game_map.shape
+        for column in range(columns):
+            for line in range(lines):
+                if game_map[column][line] != 0:
+                    self.set_cell(Renderer.GREEN, column, line)
+
+        pygame.display.update()
--- utils.py
+++ utils.py
@@ -3,29 +3,64 @@ import numpy as np
 
 
 class Point:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
 
-    def move(self, x, y):
-        self.x += x
-        self.y += y
+    def __init__(self, *args):
+        args_len = len(args)
+        if args_len == 0:
+            self.x = 0
+            self.y = 0
+        elif args_len == 1 and isinstance(args[0], Point):
+            self.x = args[0].x
+            self.y = args[0].y
+        elif args_len == 1 and isinstance(args[0], tuple):
+            self.x = args[0][0]
+            self.y = args[0][1]
+        elif args_len == 2 and isinstance(args[0], int) and isinstance(args[1], int):
+            self.x = args[0]
+            self.y = args[1]
+        else:
+            raise ValueError(
+                "takes 0 or 1 positional argument with type of <{}> "
+                "or 1 positional argument with type of <tuple> "
+                "or 2 positional arguments with type of <{}> but <{}> "
+                "were given".format("Point", "int", args))
+
+    def __str__(self):
+        return [self.x, self.y].__str__()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return self.x * self.y
 
     def move(self, offset):
         self.x += offset.x
         self.y += offset.y
+        return self
 
-    def move(self, tuple):
-        self.x += tuple[0]
-        self.y += tuple[1]
+    def set(self, other):
+        self.x = other.x
+        self.y = other.y
 
 
 class Direction(Enum):
+    def __init__(self, dx, dy):
+        self.offset = Point(dx, dy)
 
-    def __init__(self, offset):
-        self.offset = offset
-
-    UP = Point(0, 1)
-    RIGHT = Point(1, 0)
-    DOWN = Point(0, -1)
-    LEFT = Point(-1, 0)
+    LEFT = -1, 0
+    UP = 0, 1
+    RIGHT = 1, 0
+    DOWN = 0, -1
