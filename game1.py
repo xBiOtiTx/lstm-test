d599d8fba5bcc4275c4b7002fd05f285f145d470
Eugen <ieugenor@gmail.com>
2019-07-17 00:03:25 +0300

    add Agent and State to game1

--- game1.py
+++ game1.py
@@ -1,5 +1,10 @@
 import pygame
 import random
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+import random
+import numpy as np
 
 
 class Game1:
@@ -79,3 +84,107 @@ class Renderer:
         #             self.set_cell(Renderer.GREEN, column, line)
 
         pygame.display.update()
+
+
+class Agent:
+
+    def __init__(self, column, lines):
+        self.column = column
+        self.lines = lines
+        self.model = self.create_model()
+        self.action_delta = 0.1
+
+    def create_model():
+        model = Sequential()
+        model.add(Dense(output_dim=120, activation='relu'))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=120, activation='relu'))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=120, activation='relu'))
+        model.add(Dropout(0.15))
+        model.add(Dense(output_dim=4, activation='softmax'))
+        opt = Adam()
+        model.compile(loss='mse', optimizer=opt)
+        return model
+
+    def create_model():
+        model = Sequential()
+        model.add(LSTM(250, return_sequences=True))
+        model.add(LSTM(250, return_sequences=True))
+        model.add(Dropout(0.15))
+        model.add(TimeDistributed(Dense(250)))
+        model.add(Dense(output_dim=4, activation='softmax'))
+        opt = Adam()
+        model.compile(loss='mse', optimizer=opt)
+        return model
+
+    def __calc_reward(self, state_old, state_new):
+        if not state_old.gameover and state_new.gameover:
+            return -100
+
+        if state_old.score < state_new.score:
+            return 10
+
+        reward = 0
+
+        if state_new.distance_to_feed < state_old.distance_to_feed:
+            reward += 0.1
+
+        if state_new.distance_to_feed > state_old.distance_to_feed:
+            reward -= 0.1
+
+        return reward
+
+    def train(self, state_old, state_new, action):
+        target_data = self.model.predict(state_old.data)
+        target_data[0][list(Direction).index(action)] = self.__calc_reward(state_old, state_new)
+        self.model.fit(state_old.data, target_data, epochs=1, verbose=0)
+
+    def get_action(self, state):
+        prediction = self.model.predict(state.data)[0]
+
+        max_prediction = np.amax(prediction)
+        delta = max_prediction * self.action_delta
+        prediction_from = max_prediction - delta
+        prediction_to = max_prediction + delta
+
+        action_candidate_indices = np.where((prediction > prediction_from) & (prediction < prediction_to))[0]
+        action_index = random.choice(action_candidate_indices)
+        action = list(Direction)[action_index]
+
+        return action
+
+
+class State:
+
+    def __init__(self, game):
+        snake = game.snake
+        head = game.snake.segments[0]
+        food = game.food
+
+        self.bool_state = np.array([
+            snake.test_move(game.columns, game.lines, Direction.LEFT.shift),
+            snake.test_move(game.columns, game.lines, Direction.UP.shift),
+            snake.test_move(game.columns, game.lines, Direction.RIGHT.shift),
+            snake.test_move(game.columns, game.lines, Direction.DOWN.shift),
+            food.x < head.x,
+            food.x > head.x,
+            food.y < head.y,
+            food.y < head.y,
+        ])
+        self.bool_state = 1*self.bool_state.reshape((1, self.bool_state.size))
+
+        self.distance_to_feed = (head.x - game.food.x) ** 2 + (head.y - game.food.y) ** 2
+
+        game_map = [[0 for i in range(game.columns)] for j in range(game.lines)]
+        for seg in game.snake.segments:
+            game_map[seg.x][seg.y] = 1
+
+        game_map[head.x][head.y] = 2
+        game_map[game.food.x][game.food.y] = 3
+        result = np.rot90(np.array(game_map)).reshape((1, 400))
+        # result = np.c_[np.rot90(np.array(map)).reshape((1, 400)), [self.distance_to_feed]]
+
+        self.data = self.bool_state
+        self.score = game.score
+        self.gameover = game.gameover
